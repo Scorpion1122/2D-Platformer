1fc2fca173598ead0fc8b29f7f677bb138eb7ad1
Scorpion1122 <thijs.kooyers@gmail.com>
2019-12-29 15:31:46 +0100

    updated marching square package

--- Assets/_Generated/Scripts/Tags.cs
+++ Assets/_Generated/Scripts/Tags.cs
@@ -10,4 +10,11 @@ public static class Tags
 	public const string MAIN_CAMERA = "MainCamera";
 	public const string PLAYER = "Player";
 	public const string GAME_CONTROLLER = "GameController";
+	public const string GROUND = "ground";
+	public const string OBSTACLE = "Obstacle";
+	public const string BOMB_PICKUP = "BombPickup";
+	public const string ENEMY = "Enemy";
+	public const string EXPLOSION_FX = "ExplosionFX";
+	public const string BULLET = "Bullet";
+	public const string HEALTH_BAR = "HealthBar";
 }
